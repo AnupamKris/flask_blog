0434cc5800f3fed054fa2b10d4501be4ed1353bf
Anupam Krishna <anupamkris13262@gmail.com>
2020-05-02 15:35:09 +0530

    First commit

---
+++ flaskblog/forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
+
+class RegistrationForm(FlaskForm):
+	username = StringField('Username', validators=[DataRequired(),Length(min=3, max=20)])
+	email = StringField('Email',validators=[DataRequired(), Email()])
+	password = PasswordField('Password',validators=[DataRequired()])
+	confirmpassword = PasswordField('Confirm Password',
+		validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField('Sign Up')
+
+	def validate_username(self, username):
+		user = User.query.filter_by(username = username.data).first()
+		if user:
+			raise ValidationError('Username is already taken')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email = email.data).first()
+		if user:
+			raise ValidationError('User with this email already exists')
+
+
+class LoginForm(FlaskForm):
+	email = StringField('Email',validators=[DataRequired(), Email()])
+	password = PasswordField('Password',validators=[DataRequired()])
+	remember = BooleanField('Remember Me')
+	submit = SubmitField('Login')
+
